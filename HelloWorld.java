e6b12b45de9716aadc1a6ada03e10f93e44b1dc6
Konstantinos Kyprianou <t8220215@aueb.gr>
2024-11-04 20:35:39 +0200

    I added my name

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,7 +3,8 @@
 class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
-        System.out.println(AAAAAAA BOEITHIA);
+        System.out.println("AAAAAAA BOEITHIA");
         System.out.println("ΕΛΕΝΙΤΣA");
+        System.out.println("Konstantinos");
     }
 }
