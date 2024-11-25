8cb8eb841b70e26f43520e09aff6b774e9ff41e6
PanaKri <t8230065@aueb.gr>
2024-11-25 19:54:34 +0200

    Debugged code in Main

--- ProgramMain.java
+++ ProgramMain.java
@@ -1,12 +1,12 @@
 import java.util.Scanner;
 public class ProgramMain {
     public static void main (String [] args) {
-        Scanner scanner1 = new Scanner(system.in);
+        Scanner scanner1 = new Scanner(System.in);
+        boolean F = false;
         do {
             System.out.println("Press 1 for Professor");
             System.out.println("Press 2 for Student");
             System.out.println("Press 3 to exit");
-            boolean F = false;
             int Answer = scanner1.nextInt();
             if (Answer == 1 || Answer == 2 || Answer == 3) {
                 F = true;
@@ -14,5 +14,6 @@ public class ProgramMain {
                 System.out.println("Error: Expected 1, 2 or 3 as an answer.");
             }
         } while (F == false);
-    }   
+        scanner1.close();
+    }
 }
