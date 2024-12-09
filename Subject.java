9be1661b4b2715e9b434987c26268daa17cd107a
PanaKri <t8230065@aueb.gr>
2024-12-09 11:34:28 +0200

    Added Constructor and methods

--- Subject.java
+++ Subject.java
@@ -2,4 +2,21 @@ public class Subject {
     private int subjectCode;
     private int studentNumber;
     private int professorAM;
+    public int examRoomNumber;
+    public Subject(
+        int subCode, int studNumber, int proffAM, int roomNum) {
+        this.subjectCode = subCode;
+        this.studentNumber = studNumber;
+        this.professorAM = proffAM;
+        this.examRoomNumber = 0;
+    }
+    public void setExamRoomNumber(int num) {
+        this.examRoomNumber = num;
+    }
+    public void getInfo() {
+        System.out.println("this is subject " + subjectCode
+        + " is taught by " + professorAM + " with enrolled " 
+        + studentNumber + " students" + " taken in room "
+        + examRoomNumber);
+    }
 }
