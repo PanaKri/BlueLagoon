a9045739a6fc23464d85781fde2d9d06530d4001
PanaKri <t8230065@aueb.gr>
2025-01-12 11:12:22 +0200

    Updated Room and Subject class

--- Room.java
+++ Room.java
@@ -1,19 +1,22 @@
 public class Room {
-    public static int numberOfRooms = 0;
     private int numberOfSeats;
     private int roomNumber;    
     public boolean availability[][];
+    public static Room[] rooms;
+    public static int roomCount = 0;
     // Constructor
-    public Room(int numberOfSeats, int roomNumber, String selectedInstructor) {
-        numberOfRooms += 1;
+    public Room(int numberOfSeats, String selectedInstructor) {
         this.numberOfSeats = numberOfSeats;
-        this.roomNumber = numberOfRooms;
+        this.roomNumber = roomCount;
         this.availability = new boolean[5][5];
         for (int index = 0; index < availability.length; index++) {
             for (int index1 = 0; index1 < availability.length; index++) {
                 availability[index][index1] = true;
             }
         }
+        rooms[roomCount] = this;
+        this.roomNumber = roomCount;
+        roomCount++;
     }
 
     public int getNumberOfSeats() {
--- Subject.java
+++ Subject.java
@@ -3,15 +3,33 @@ public class Subject {
     private int studentNumber;
     private int professorAM;
     public int examRoomNumber;
-    public Subject(
-        int subCode, int studNumber, int proffAM, int roomNum) {
+    private int examDate;
+    private int examHour;
+    public static Subject[] subjects;
+    public static int subjectCount = 0;
+    public Subject(int subCode, int studNumber, int proffAM, int roomNum) {
         this.subjectCode = subCode;
         this.studentNumber = studNumber;
         this.professorAM = proffAM;
         this.examRoomNumber = 0;
+        subjects[subjectCount] = this;
+        subjectCount++;
     }
     public void setExamRoomNumber(int num) {
         this.examRoomNumber = num;
+    }
+    public void setExamHour(int num) {
+        this.examHour = num;
+    }public void setExamDate(int num) {
+        this.examDate = num;
+    }
+    public int getExamHour() {
+        return examHour;
+    }
+    public int getExamDate() {
+        return examDate;
+    }    public int getExamRoomNumber() {
+        return examRoomNumber;
     }
     public void getInfo() {
         System.out.println("this is subject " + subjectCode
