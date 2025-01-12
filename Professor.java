6eeec6c683ebd9a0f7ccd1564365f4a1c84b65e1
PanaKri <t8230065@aueb.gr>
2025-01-12 10:57:20 +0200

    Updated Professor class

--- Professor.java
+++ Professor.java
@@ -1,14 +1,10 @@
 import java.time.LocalDate;
-import java.util.Arrays;
-
 public class Professor {
-
     private String name;
     private String surname;
     private int am;
     private String[] courses;
     private LocalDate[] examDates;
-<<<<<<< HEAD
     public static Professor[] proffessors;
     public static int profCount = 0;
     public Professor(String name, String surname, int am, String[] courses) {
@@ -16,27 +12,15 @@ public class Professor {
         this.surname=surname;
         this.am=am;
         this.courses=courses;
-=======
-
-    public Professor(String name, String surname, int am, String... courses) {
-        this.name = name;
-        this.surname = surname;
-        this.am = am;
-        this.courses = courses;
->>>>>>> 2d5af314780f8a7bbadac41eec4584fec3f22f0e
         this.examDates = new LocalDate[courses.length];
         proffessors[profCount] = this;
         profCount++;
     }
-
     public void assignExamDate(int index, LocalDate date) {
         if (index >= 0 && index < courses.length) {
             examDates[index] = date;
-        } else {
-            System.err.println("Error: Invalid index for exam date assignment.");
         }
     }
-
     public void displayInformation() {
         System.out.println("Όνομα καθηγητή:" + name);
         System.out.println("Επίθετο καθηγητή:" + surname);
@@ -46,35 +30,4 @@ public class Professor {
             System.out.println("-" + courses[i] + ":" + (examDates[i]!= null ? examDates[i] : "Not set"));
         }
     }    
-
-    public String getName() {
-        return name;
-    }
-
-    public String getSurname() {
-        return surname;
-    }
-
-    public int getAm() {
-        return am;
-    }
-
-    public String[] getCourses() {
-        return courses.clone();
-    }
-
-    public LocalDate[] getExamDates() {
-        return examDates.clone();
-    }
-
-    @Override
-    public String toString() {
-        return "Professor{" +
-                "name='" + name + '\'' +
-                ", surname='" + surname + '\'' +
-                ", am=" + am +
-                ", courses=" + Arrays.toString(courses) +
-                ", examDates=" + Arrays.toString(examDates) +
-                '}';
-    }
 }
