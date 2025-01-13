8e2e0dbfa8b36474be4681a59b53e7b26b86f019
ELENA PAPAIOANNOU <t8230116@aueb.gr>
2025-01-13 17:31:29 +0200

    made some changes

--- JAVA.sql
+++ JAVA.sql
@@ -6,7 +6,12 @@ DROP TABLE ROOM;*/
 DROP TABLE COURSE;
 CREATE TABLE COURSE(
 course_id INTEGER PRIMARY KEY,
-course_name VARCHAR(255) NOT NULL
+course_name VARCHAR(255) NOT NULL,
+professor_am INTEGER NOT NULL,
+exam_room INTEGER NOT NULL,
+student_number INTEGER NOT NULL,
+exam_date DATETIME NOT NULL,
+exam_time TIME NOT NULL
 );
 DROP TABLE ROOM;
 CREATE TABLE ROOM(
@@ -14,16 +19,16 @@ room_id INTEGER PRIMARY KEY,
 room_quantity INTEGER
 );
 
-INSERT INTO COURSE (course_id, course_name)
-VALUES (1, "CODING");
-INSERT INTO COURSE (course_id, course_name)
-VALUES (2, "MANAGEMENT");
-INSERT INTO COURSE (course_id, course_name)
-VALUES (3, "MARKETING");
-INSERT INTO COURSE (course_id, course_name)
-VALUES (4, "STATISTICS");
-INSERT INTO COURSE (course_id, course_name)
-VALUES (5, "LOGISTICS");
+INSERT INTO COURSE (course_id, course_name, professor_am, exam_room, student_number, exam_date, exam_time)
+VALUES (1, "CODING", 4569871, 1, 120, "2025-01-24", "13:00:00");
+INSERT INTO COURSE (course_id, course_name, professor_am, exam_room, student_number, exam_date, exam_time)
+VALUES (2, "MANAGEMENT", 8759546, 2, 100, "2025-01-27", "15:00:00");
+INSERT INTO COURSE (course_id, course_name, professor_am, exam_room, student_number, exam_date, exam_time)
+VALUES (3, "MARKETING", 7546269, 3, 95, "2025-01-29", "14:00:00");
+INSERT INTO COURSE (course_id, course_name, professor_am, exam_room, student_number, exam_date, exam_time)
+VALUES (4, "STATISTICS", 6301587, 4, 74, "2025-02-02", "12:00:00");
+INSERT INTO COURSE (course_id, course_name, professor_am, exam_room, student_number, exam_date, exam_time)
+VALUES (5, "LOGISTICS", 1895762, 5, 89, "2025-02-05", "10:00:00");
 
 INSERT INTO ROOM (room_id, room_quantity)
 VALUES (1,100);
