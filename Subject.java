b264ef29ec70aa55f81e8d7527073537c884975d
vasilikiz5 <t8230036@aueb.gr>
2025-01-13 20:19:26 +0200

    changed the constructor so it aligns with the ui

--- Subject.java
+++ Subject.java
@@ -1,20 +1,26 @@
 public class Subject {
     private int subjectCode;
     private int studentNumber;
-    private String subjectName;
+    private String courseName;
     private int professorAM;
+    public int examRoomNumber;
+    private String examDate;
+    private String examTime;
     public static Subject[] subjects;
     public static int subjectCount = 0;
-    public Subject(int subCode, int studNumber, int proffAM, int roomNum, String name) {
-        this.subjectName = name;
-        this.subjectCode = subCode;
+    public Subject(int subjectCode, int studNumber, int professorAM, int examrRoomNum, String courseName, String examDate, String examTime) {
+        this.courseName = courseName;
+        this.subjectCode = subjectCode;
         this.studentNumber = studNumber;
-        this.professorAM = proffAM;
+        this.professorAM = professorAM;
+        examRoomNumber = 0;
+        this.examDate = examDate;
+        this.examTime = examTime;
         subjects[subjectCount] = this;
         subjectCount++;
     }
     public String getSubjectName() {
-        return this.subjectName;
+        return this.courseName;
     }
     public int getSubjectCode() {
         return this.subjectCode;
@@ -22,9 +28,13 @@ public class Subject {
     public int getStudentNumber() {
         return studentNumber;
     }
+    public int getProfessorAm() {
+        return professorAM;
+    }
     public void getInfo() {
         System.out.println("this is subject " + subjectCode
         + " is taught by " + professorAM + " with enrolled " 
-        + studentNumber + " students");
+        + studentNumber + " students" + " taken in room "
+        + examRoomNumber);
     }
 }
