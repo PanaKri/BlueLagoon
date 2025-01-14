a2ba64ebebf9fbffa759fa3707cd2f114a4a4857
chrysfed <t8230167@aueb.gr>
2025-01-14 14:58:38 +0200

    added prof and student

--- JAVA.sql
+++ JAVA.sql
@@ -1,13 +1,27 @@
 CREATE DATABASE JAVA;
 USE JAVA;
 
+CREATE TABLE STUDENT(
+student_id INT PRIMARY KEY,
+student_name VARCHAR(30),
+student_surname VARCHAR(30),,
+course_id INT REFERENCES COURSE(course_id), /*this needs to be added after the table is created with ALTER TABLE STUDENT ADD course_id INT REFERENCES COURSE(course_id);*/
+);
+
+CREATE TABLE PROFESSOR(
+professor_am INT PRIMARY KEY,
+professor_name VARCHAR(30),
+professor_surname VARCHAR(30),
+);
+
+
 /*DROP TABLE COURSE
 DROP TABLE ROOM;*/
 CREATE TABLE COURSE(
 course_id INTEGER PRIMARY KEY,
 course_name VARCHAR(255) NOT NULL,
-professor_am INTEGER NOT NULL,
-student_number INTEGER NOT NULL,
+professor_am INT REFERENCES PROFESSOR(professor_am),
+student_id INT REFERENCES STUDENT(student_id),
 );
 DROP TABLE ROOM;
 CREATE TABLE ROOM(
@@ -37,4 +51,10 @@ VALUES (4,60);
 INSERT INTO ROOM (room_id, room_quantity)
 VALUES (5,90);
 
+INSERT INTO STUDENT (student_id, student_name, student_surname, course_id)
+VALUES (12345, "GIORGOS", "PAPADOPOULOS", "CODING");
+INSERT INTO STUDENT (student_id, student_name, student_surname, course_id)
+VALUES (67890, "MARIA", "ALEXIOU", "MANAGEMENT");
 
+INSERT INTO PROFESSOR (professor_am, professor_name, professor_surname)
+VALUES (4569871, "SAKIS", "ROUBAS");
