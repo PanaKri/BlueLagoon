f21d24ddb957b62f104906fa7cc872809f65e4d0
PanaKri <t8230065@aueb.gr>
2025-01-14 21:33:12 +0200

    Possible solution to exception

---
+++ DB.java
@@ -0,0 +1,9 @@
+public class DB {
+    public static void AddInfo() {
+        Room room1 = new Room(128);
+        Room room2 = new Room(134);
+        Room room3 = new Room(100);
+        Room room4 = new Room(96);
+        Room room5 = new Room(90);
+    } 
+}
