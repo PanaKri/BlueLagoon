abc61f837c03f58b556c68003348653b10fcceef
PanaKri <t8230065@aueb.gr>
2025-01-17 10:18:23 +0200

    Solved exceptions in main and updated terminal output

---
+++ Terminal/src/main/java/BlueLagoonTerminal/Student.java
@@ -0,0 +1,54 @@
+package BlueLagoonTerminal;
+public class Student {
+    private String name;
+    private String surname;
+    private int studentID;
+    private int yearOfAdmission;
+    private int numbOfCourses = 0;
+    private String[] courses = new String[5];
+    public static Student[] students= new Student[10];
+    public static int studCount = 0;
+    public Student(String name, String surname, int studentID, int yearOfAdmission) {
+       this.name = name;
+       this.surname = surname;
+       this.studentID = studentID;
+       this.yearOfAdmission = yearOfAdmission; 
+       for (int i = 0; i < 5; i++) {
+        courses[i] = "None";
+       }
+       students[studCount] = this;
+       studCount = studCount + 1;
+    }
+    public String getEnrolled(String s) {
+        if (numbOfCourses < 5) {
+            courses[numbOfCourses] = s;
+            numbOfCourses = numbOfCourses + 1;
+            return "You have enrolled in:" + " " + s;
+        } else {
+            return "Max amount of courses reached";
+        }
+    }
+    public String displayInfo() {
+        return name + " " + surname + " " + "has student ID: " + studentID
+        + "and got registered in: " + yearOfAdmission;
+    }
+    public String displayCourses() {
+        String s = courses[0];
+        int i = 1;
+        boolean f = false;
+        if (courses[i] != "None") {
+            do {
+                s = s + courses[i] + " ";
+                i = i + 1;
+                if (i == 5) {
+                    i = 0;
+                    f = true;
+                }
+            } while (courses[i] != "None" & i < 5 & f == false);
+        }
+        return s;
+    }
+    public int getAM() {
+        return this.studentID;
+    }
+}
---
+++ Terminal/src/main/java/BlueLagoonTerminal/StudentManagerSystem.java
@@ -0,0 +1,54 @@
+package BlueLagoonTerminal;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+public class StudentManagerSystem {
+    Scanner input = new Scanner(System.in);
+    private String choice;
+    public int identify() {
+        try {
+            System.out.println("");
+            System.out.println("Type your AM");
+            int am = input.nextInt();
+            input.nextLine();
+            int AM = -1;
+            for (int i = 0; i < Student.studCount; i++) {
+                Student student = Student.students[i];
+                if (student.getAM() == am) {
+                    AM = i;
+                }
+            }
+            return AM;
+        } catch (InputMismatchException e) {
+            input.nextLine();
+            return -1;
+        }
+    }
+    public void addSubject(Student stud) {
+        System.out.println("Please state which subject you want to get enrolled");
+        choice = input.nextLine();
+        boolean flag = false;
+        int i = 0;
+        Subject selection = Subject.subjects[i];
+        do {
+            Subject sub = Subject.subjects[i];
+            String name = sub.getSubjectName();
+            if (name.equals(choice)) {
+                flag = true;
+                selection = sub;
+            } else {
+                i = i + 1;
+            }
+        } while (flag == false & i < Subject.subjectCount);
+        if (flag == true) {
+            String answer = stud.getEnrolled(choice);
+            selection.addStudent();
+            System.out.println(answer);
+        } else {
+            System.out.println("No subject named " + choice);
+        }
+    }
+    public void DisplayInfo(Student stud) {
+        System.out.println(stud.displayInfo());
+        System.out.println("Courses taken " + stud.displayCourses());
+    }
+}
